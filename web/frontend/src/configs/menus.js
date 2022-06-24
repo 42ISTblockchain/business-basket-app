ab0707f6e37b8a277f688e9b740b6dee166e3c14
Fatih Karatay <fkaratay@k1m05s02.42istanbul.com.tr>
2022-06-25 00:32:40 +0300

    Layout created

---
+++ web/frontend/src/configs/menus.js
@@ -0,0 +1,22 @@
+export const menus = {
+    "hire" : [
+        {
+            id: 1,
+            name: 'Anasayfa',
+            path: '/',
+            component: 'Dashboard'
+        },
+        {
+            id: 2,
+            name: 'İş Listem',
+            path: '/job',
+            component: 'Job'
+        },
+        {
+            id: 3,
+            name: 'Başvurular',
+            path: '/applications',
+            component: 'Application'
+        },
+    ]
+}
